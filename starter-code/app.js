ad2205fbf5ecb43b4b1278a63c16f3d3fa176454
hayleytaylor29 <hft929@gmail.com>
2021-03-30 17:33:33 -0400

    final to-do list

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,20 +7,22 @@ $(() => {
      'brush my fang',
      'water the hardwoods'
    ]
+   
    //function - when the 'remove' button is clicked, remove the item from the list
     //am not able to get this part to work for some reason, and when I click on the
     //remove button on an item that isn't the last item in the list, they switch
     //orders, I've played around with this a lot and am not sure what's going on 
     //only when I add a new to do item and add that to the done list, the item above the
-  //newly added to-do will remove when you clicking the remove button, but only when I add the new item
-  //at a certain, point, really not sure what happened here!!
-    var $remove = () => {
-      $('.removeButton').click(function(event){
-        let $removeItem = $(event.currentTarget).parent();
-        $removeItem.remove();
-        }
-      )}
-      $remove();
+    //newly added to-do will remove when you clicking the remove button, but only when I add the new item
+    //last and everything else is added before, this one is driving me nuts I feel like I'm so close!!
+      var $remove = () => {
+        $('.removeButton').click(function(event){
+          let $removeItem = $(event.currentTarget).parent();
+          $removeItem.remove();
+          }
+        )}
+        $remove();
+        
 
   //function - when the completed button is clicked the item moves from the to-do list to
   //the completed list, the new class is added and the text of the button changes from
@@ -33,12 +35,11 @@ $(() => {
         $(event.currentTarget).text('remove');
         $doneItem.removeClass('to-do-item').addClass('done-item');
         $('#completed').append($doneItem);
-        }
+      }
       )};
       $complete();
       $remove();
-    
-  
+      
     //function - loop through the array of given to-do's and add each one to the to-do list as
     //an individual div with a complete button, assign the correct class
     const $addList = () => {
@@ -54,10 +55,10 @@ $(() => {
         }
     }
     $addList();
-    $complete()
-    $remove()
+    $complete();
+    $remove();
     
-
+    
     
     //function - when you type a new to-do item into the text box and click add, the new item will
     //be added to the to-do list with the completed button, the text box then clears. If no text is 
@@ -73,10 +74,9 @@ $(() => {
           $toDo.append($newDiv);
           $newDiv.append($newItem);
           $button.insertAfter($newItem);
-          
+      
           $complete();
           $remove();
-
       }
           else {
             alert('enter a new to-do item, silly goose!');
